cf2e8fdc6079a2c9fb486b75bb82576300868854
Pushpak Panchal <pushpak.net@gmail.com>
2020-08-16 00:43:54 -0500

    adding pdf stuff

--- MedAPI-UI/src/app/components/note/form-summary/form-summary.component.ts
+++ MedAPI-UI/src/app/components/note/form-summary/form-summary.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { jsPDF } from "jspdf";
+import 'jspdf-autotable';
+import { CommonService } from 'src/app/services/common.service';
+
 
 @Component({
   selector: 'app-form-summary',
@@ -10,11 +14,18 @@ export class FormSummaryComponent implements OnInit {
   @Input() note: any;
   @Input() patient: any;
 
-  constructor() { }
+  constructor(private commonService: CommonService) { }
 
   ngOnInit(): void {
   }
 
   
-  
+  downloadAttention(){
+
+    this.commonService.generateAttentionPDF(this.patient, this.note);
+  }
+
+  downloadPrescription(){}
+
+  downloadInter(){}
 }
--- MedAPI-UI/src/app/components/record/record.component.ts
+++ MedAPI-UI/src/app/components/record/record.component.ts
@@ -23,6 +23,7 @@ import { PersonalBackgroundList } from '../../models/personalBackgroundList.mode
 import { CardiovascularSymptoms } from '../../models/cardiovascularSymptoms.model';
 import { jsPDF } from "jspdf";
 import 'jspdf-autotable';
+import { CommonService } from 'src/app/services/common.service';
 
 
 export enum TicketStatus {
@@ -68,7 +69,7 @@ export class RecordComponent implements OnInit {
   { value: 'PEDIATRY', name: 'Pediatría', id: 3 },
   { value: 'TRAUMATOLOGY', name: 'Traumatología', id: 4 }];
 
-  constructor(private recordService: RecordService, public router: Router, private changeDetectorRefs: ChangeDetectorRef) { }
+  constructor(private recordService: RecordService, public router: Router, private changeDetectorRefs: ChangeDetectorRef, private commonService : CommonService) { }
 
   ngOnInit(): void {
     this.askTicket = false;
@@ -86,153 +87,7 @@ export class RecordComponent implements OnInit {
 
 
   downloadAttentionPdf(note: NoteDetail) {
-
-    try {
-      var doc = new jsPDF();
-     
-      doc.setFontSize(35);
-      doc.text("Atención medica", 14, 15);
-      doc.setFontSize(20);
-
-      //Name
-      doc.setFont("helvetica", "bold");
-      doc.text("Paciente", 14, 30);
-
-      doc.setFont("helvetica", null);
-      doc.text(this.patient.name, 14, 38);
-
-      // Patient Doc Number
-      doc.setFont("helvetica", "bold");
-      doc.text("DNI", 14, 50);
-
-      doc.setFont("helvetica", null);
-      doc.text(this.patient.documentNumber, 14, 58);
-
-      // Patient Background Information
-      doc.setFont("helvetica", "bold");
-      doc.text("Relato", 14, 70);
-  
-      if(note.symptoms.background)
-      {
-        doc.setFont("helvetica", null);
-        doc.text(note.symptoms.background, 14, 78);
-      }
-
-      //Note Examen físico
-      doc.setFont("helvetica", "bold");
-      doc.text("Examen físico", 14, 95);
-
-      doc.setFont("helvetica", null);
-      doc.text(note.physicalExam.text, 14, 103);
-
-      //Diagnóstico
-      doc.setFont("helvetica", "bold");
-      doc.text("Diagnóstico", 14, 130);
-
-      doc.setFont("helvetica", null);
-      doc.text("", 14, 123);
-
-      var col = ["#", "CIE-10", "Descripción", "Tipo"];
-      var rows = [];
-
-      for (var i = 0; i < note.diagnosis.list.length; i++) {
-
-        var temp = [i + 1, note.diagnosis.list[i].code, note.diagnosis.list[i].title, note.diagnosis.list[i].type];
-        rows.push(temp);
-
-      }
-
-      doc.autoTable({
-        styles: { theme: 'plain' },
-        margin: { top: 135 },
-        body: rows,
-        columns: col,
-        theme: 'grid',
-        headStyles: { fontSize: 18, fontStyle: 'bold', fillColor: 'white', textColor: 'black', lineColor: 'black', lineWidth: 0.5 },
-        bodyStyles: { fontSize: 18, textColor: 'black', lineColor: 'black', lineWidth: 0.5 },
-        didDrawPage: function (data) {
-
-          //console.log(data);
-          // diagossisHeight = data.table.finalY
-
-        }
-
-      });
-
-      //Examenes solicitados
-
-      //  doc.autoPrint()
-
-      //  doc.autoTable({
-      //   margin: { top: 135 },
-      //   body: [],
-      //   columns: ['Examenes solicitados'],
-      //   theme : 'plain',
-      //   headStyles : {fontSize : 18, fontStyle : 'bold', fillColor : 'white', textColor : 'black'},
-      //   bodyStyles : {fontSize : 18,  textColor : 'black'}
-
-      // });
-
-      doc.setFont("helvetica", "bold");
-      doc.text("Examenes solicitados", 14, 12 + doc.lastAutoTable.finalY); // The y position on the page
-
-
-      //  doc.setFont("helvetica",null);
-      //  doc.text("", 14, 190);
-
-      var colExams = ["#", "Nombre", "Especificación"];
-      var rowsExam = [];
-
-
-      for (var i = 0; i < note.exams.list.length; i++) {
-
-        var temp = [i + 1, note.exams.list[i].name, "-"];
-        rowsExam.push(temp);
-
-      }
-
-
-      doc.autoTable({
-        styles: { theme: 'plain' },
-        margin: { top: 190 },
-        startY: doc.lastAutoTable.finalY + 17,
-        body: rowsExam,
-        columns: colExams,
-        theme: 'grid',
-        headStyles: { fontSize: 18, fontStyle: 'bold', fillColor: 'white', textColor: 'black', lineColor: 'black', lineWidth: 0.5 },
-        bodyStyles: { fontSize: 18, textColor: 'black', lineColor: 'black', lineWidth: 0.5 }
-
-      });
-
-      doc.setFont("helvetica", "bold");
-      doc.text("Médico", 14, 12 + doc.lastAutoTable.finalY); // The y position on the page
-
-      var medicData = JSON.parse(localStorage.getItem("userData"));
-      var medicName = medicData["name"];
-
-      doc.setFont("helvetica", "");
-      doc.text(medicName, 14, 22 + doc.lastAutoTable.finalY);
-
-      //Patient Sex
-      doc.setFont("helvetica", "bold");
-      doc.text("Sexo", 156, 30);
-
-      doc.setFont("helvetica", null);
-      doc.text(this.patient.sex, 156, 38);
-
-      // Patient Date of Birth
-      doc.setFont("helvetica", "bold");
-      doc.text("DNI", 156, 50);
-
-
-      doc.setFont("helvetica", null);
-      doc.text(this.patient.documentNumber, 156, 58);
-
-      doc.save('Test.pdf');
-    }
-    catch (err) {
-      console.log(err);
-    }
+    this.commonService.generateAttentionPDF(this.patient,note);
   }
 
   searchTicket() {
