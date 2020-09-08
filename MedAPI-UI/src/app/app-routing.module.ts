862e5869ee53a92508ca45472177a27b6516b148
Pushpak Panchal <pushpak.net@gmail.com>
2020-09-07 21:49:24 -0500

    patient registeration

--- MedAPI-UI/src/app/app-routing.module.ts
+++ MedAPI-UI/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthModule } from './auth/auth.module';
 import { MedicRegistrationComponent } from './components/user/medic-registration/medic-registration.component';
+import { PatientRegistrationComponent } from './components/user/patient-registration/patient-registration.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: '', loadChildren: () => import('../app/components/main/main.module').then(m => m.MainModule) },
   { path: 'login', loadChildren: () => import('../app/components/user/user.module').then(m => m.UserModule) },
-  { path: 'register', component : MedicRegistrationComponent }
+  { path: 'register', component : MedicRegistrationComponent },
+  { path: 'patient-register', component : PatientRegistrationComponent }
   //{ path: '**', redirectTo: 'login', data: { isRedirect: true } }
  ];
 
--- MedAPI-UI/src/app/components/user/patient-registration/patient-registration.component.ts
+++ MedAPI-UI/src/app/components/user/patient-registration/patient-registration.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { PatientService } from '../../patient/service/patient.service';
+import { ToastrService } from 'ngx-toastr';
+import { MedicUser } from 'src/app/models/medicuser.model';
+import { Medic } from 'src/app/models/medic.model';
 
 @Component({
   selector: 'app-patient-registration',
@@ -6,10 +11,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./patient-registration.component.scss']
 })
 export class PatientRegistrationComponent implements OnInit {
+  resources: any;
+  medic: Medic = new Medic();
 
-  constructor() { }
+  constructor(public router: Router, private patientService: PatientService, public toastr: ToastrService) {
+    this.medic.user = new MedicUser();
+    this.medic.user.roleId = 4;
+   }
 
   ngOnInit(): void {
+    let resourcesPath: string = 'users/resources';
+
+    this.patientService.getResources(resourcesPath).then((response: any) => {
+      this.patientService.resources.next(response);
+    }).catch((error) => {
+      console.log(error);
+    });
+
+    this.patientService.resources.subscribe((o) => {
+      this.resources = o;
+    });
   }
 
 }
