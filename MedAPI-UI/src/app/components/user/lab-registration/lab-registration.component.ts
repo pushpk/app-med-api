852a78f0089320ffa00918d487c253174166bdd7
Pushpak Panchal <pushpak.net@gmail.com>
2020-09-23 22:42:07 -0500

    a

--- MedAPI-UI/src/app/components/user/lab-registration/lab-registration.component.ts
+++ MedAPI-UI/src/app/components/user/lab-registration/lab-registration.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { LabUser } from 'src/app/models/labUser.model';
+import { MedicUser } from 'src/app/models/medicuser.model';
 import { PatientService } from '../../patient/service/patient.service';
 
 @Component({
@@ -11,9 +12,11 @@ import { PatientService } from '../../patient/service/patient.service';
 })
 export class LabRegistrationComponent implements OnInit {
 
-  medic: LabUser = new LabUser();
+  labUser: LabUser = new LabUser();
 
   constructor(public router: Router, private patientService: PatientService, public toastr: ToastrService) { 
+    this.labUser.user = new MedicUser();
+    this.labUser.user.roleId = 2;
   }
 
   ngOnInit(): void {
