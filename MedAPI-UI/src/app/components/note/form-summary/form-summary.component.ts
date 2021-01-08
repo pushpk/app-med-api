729405097dd65d820ea0e50ea24495f614ea4914
Pushpak Panchal <pushpak.net@gmail.com>
2021-01-07 23:05:17 -0600

    adding sign changes

--- MedAPI-UI/src/app/components/note/form-summary/form-summary.component.ts
+++ MedAPI-UI/src/app/components/note/form-summary/form-summary.component.ts
@@ -1,39 +1,56 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { jsPDF } from "jspdf";
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { jsPDF } from 'jspdf';
 import 'jspdf-autotable';
 import { CommonService } from 'src/app/services/common.service';
-
+import SignaturePad from 'signature_pad';
 
 @Component({
   selector: 'app-form-summary',
   templateUrl: './form-summary.component.html',
-  styleUrls: ['./form-summary.component.scss']
+  styleUrls: ['./form-summary.component.scss'],
 })
 export class FormSummaryComponent implements OnInit {
   @Input() submit: any;
   @Input() note: any;
   @Input() patient: any;
   @Input() isEditable: boolean;
+  @ViewChild('sPad', { static: true }) signaturePadElement;
+  signaturePad: any;
 
-  constructor(private commonService: CommonService) { }
+  constructor(private commonService: CommonService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    this.signaturePad = new SignaturePad(
+      this.signaturePadElement.nativeElement
+    );
+  }
+  clear() {
+    this.signaturePad.clear();
   }
 
-  
-  downloadAttention(){
-
-    this.commonService.generatePDF(this.patient, this.note, "Attention");
+  radioChange(e: any) {
+    console.log(
+      '🚀 ~ file: form-summary.component.ts ~ line 35 ~ FormSummaryComponent ~ radioChange ~ e',
+      e
+    );
   }
 
-  downloadPrescription(){
-
-    this.commonService.generatePDF(this.patient, this.note, "Prescription");
+  downloadAttention() {
+    this.commonService.generatePDF(this.patient, this.note, 'Attention');
   }
 
-  downloadInter(){
+  downloadPrescription() {
+    this.commonService.generatePDF(this.patient, this.note, 'Prescription');
+  }
 
-    console.log(this.patient)
-    this.commonService.generatePDF(this.patient, this.note, "Interconsultation");
+  downloadInter() {
+    console.log(this.patient);
+    this.commonService.generatePDF(
+      this.patient,
+      this.note,
+      'Interconsultation'
+    );
   }
 }
