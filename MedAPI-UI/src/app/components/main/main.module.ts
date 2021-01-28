170fb2de385c9557c31a5503f6dedd27ee785532
Pushpak Panchal <pushpak.net@gmail.com>
2021-01-27 18:19:52 -0600

    lab upload result -work in progress

--- MedAPI-UI/src/app/components/main/main.module.ts
+++ MedAPI-UI/src/app/components/main/main.module.ts
@@ -4,24 +4,35 @@ import { MainComponent } from './main.component';
 import { SidepanelComponent } from './sidepanel/sidepanel.component';
 import { Routes, RouterModule } from '@angular/router';
 import { SharedModule } from '../../shared/shared.module';
+import { AuthGuardService } from 'src/app/auth/auth-guard.service';
 
 const routes: Routes = [
   {
-    path: '', component: MainComponent, children: [
-      { path: 'admin', loadChildren: () => import('../admin/admin.module').then(m => m.AdminModule) },
-      { path: 'records', loadChildren: () => import('../record/record.module').then(m => m.RecordModule) },
-      { path: 'patients', loadChildren: () => import('../patient/patient.module').then(m => m.PatientModule) }
-
-    ]
-  }
+    path: '',
+    component: MainComponent,
+    children: [
+      {
+        path: 'admin',
+        loadChildren: () =>
+          import('../admin/admin.module').then((m) => m.AdminModule),
+      },
+      {
+        path: 'records',
+        loadChildren: () =>
+          import('../record/record.module').then((m) => m.RecordModule),
+        canActivate: [AuthGuardService],
+      },
+      {
+        path: 'patients',
+        loadChildren: () =>
+          import('../patient/patient.module').then((m) => m.PatientModule),
+      },
+    ],
+  },
 ];
 
 @NgModule({
   declarations: [MainComponent, SidepanelComponent],
-  imports: [
-    CommonModule,
-    RouterModule.forChild(routes),
-    SharedModule
-  ]
+  imports: [CommonModule, RouterModule.forChild(routes), SharedModule],
 })
-export class MainModule { }
+export class MainModule {}
